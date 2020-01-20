6110371c2c8413604ab40cf58a04acce0ceddc86
Jon Jagger <jon@jaggersoft.com>
2020-01-20 16:44:23 +0000

    Dont trigger dependent repos yet

--- build_test_tag_publish.sh
+++ build_test_tag_publish.sh
@@ -4,4 +4,4 @@ sh_dir() { echo "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd
 source "$(sh_dir)/build_docker_images.sh"
 source "$(sh_dir)/tag_image.sh"
 source "$(sh_dir)/on_ci_publish_tagged_image.sh"
-source "$(sh_dir)/on_ci_trigger_dependent_images.sh"
+#source "$(sh_dir)/on_ci_trigger_dependent_images.sh"
