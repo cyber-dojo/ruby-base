15029d1f764255a3af43ceddbeb311c657096446
Jon Jagger <jon@jaggersoft.com>
2020-07-04 13:53:45 +0100

    Turn of repo-triggering in build script

--- build_test_tag_publish.sh
+++ build_test_tag_publish.sh
@@ -81,49 +81,6 @@ on_ci_publish_tagged_image()
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
-on_ci_trigger_dependent_images()
-{
-  if ! on_ci; then
-    echo 'not on CI so not triggering dependent images'
-    return
-  fi
-  echo 'on CI so triggering dependent images'
-  curl_trigger_script
-  local -r from_org=cyber-dojo
-  local -r from_repo=ruby-base
-  local -r from_sha="$(git_commit_sha)"
-  local -r to_org=cyber-dojo
-  local -r to_repos=rack-base
-  $(trigger_script_path) \
-    "${from_org}" "${from_repo}" "${from_sha}" \
-    "${to_org}" "${to_repos}"
-}
-
-# - - - - - - - - - - - - - - - - - - - - - - - -
-curl_trigger_script()
-{
-  local -r raw_github_org=https://raw.githubusercontent.com/cyber-dojo
-  local -r repo=cyber-dojo
-  local -r branch=master
-  local -r path=sh/circle-ci
-  local -r url="${raw_github_org}/${repo}/${branch}/${path}/$(trigger_script_name)"
-  curl --fail --output $(trigger_script_path) --silent "${url}"
-  chmod 700 "$(trigger_script_path)"
-}
-
-# - - - - - - - - - - - - - - - - - - - - - - - -
-trigger_script_path()
-{
-  echo "${TMP_DIR}/$(trigger_script_name)"
-}
-
-# - - - - - - - - - - - - - - - - - - - - - - - -
-trigger_script_name()
-{
-  echo github_automated_commit_push.sh
-}
-
-# - - - - - - - - - - - - - - - - - - - - - - - -
 on_ci()
 {
   set +u
@@ -138,4 +95,3 @@ build_docker_images
 assert_equal SHA "$(git_commit_sha)" "$(image_sha)"
 tag_image
 on_ci_publish_tagged_image
-on_ci_trigger_dependent_images
