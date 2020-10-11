1832e740f4953cee4413f4a8de172b44a92cc2aa
Jon Jagger <jon@jaggersoft.com>
2020-10-11 13:14:02 +0100

    Run CI script with docker login already in place

--- build_test_publish.sh
+++ build_test_publish.sh
@@ -73,11 +73,8 @@ on_ci_publish_tagged_image()
   echo 'on CI so publishing tagged image'
   local -r image="$(image_name)"
   local -r tag="$(image_tag)"
-  # DOCKER_USER, DOCKER_PASS are in ci context
-  echo "${DOCKER_PASS}" | docker login --username "${DOCKER_USER}" --password-stdin
   docker push ${image}:latest
   docker push ${image}:${tag}
-  docker logout
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
