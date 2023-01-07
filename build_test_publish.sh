e9109039adedff1e643d18243b04bcb0eabbb2c4
Jon Jagger <jon@jaggersoft.com>
2023-01-07 08:00:14 +0000

    Added github workflow with kosli reporting

--- build_test_publish.sh
+++ build_test_publish.sh
@@ -1,9 +1,11 @@
-#!/bin/bash -Eeu
+#!/usr/bin/env bash
+set -Eeu
 
-readonly ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-readonly TMP_DIR="$(mktemp -d /tmp/ruby-base.XXXXXXX)"
-remove_tmp_dir() { rm -rf "${TMP_DIR}" > /dev/null; }
-trap remove_tmp_dir INT EXIT
+
+export ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+export SH_DIR="${ROOT_DIR}/sh"
+
+source "${SH_DIR}/kosli.sh"
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
 build_docker_images()
@@ -88,3 +90,6 @@ build_docker_images
 assert_equal SHA "$(git_commit_sha)" "$(image_sha)"
 tag_image
 on_ci_publish_tagged_image
+on_ci_kosli_declare_pipeline
+on_ci_kosli_report_artifact
+on_ci_kosli_report_synk_evidence
---
+++ sh/kosli.sh
@@ -0,0 +1,120 @@
+#!/usr/bin/env bash
+set -Eeu
+
+# KOSLI_OWNER is set in CI
+# KOSLI_API_TOKEN is set in CI
+# KOSLI_PIPELINE is set in CI
+# KOSLI_HOST_STAGING is set in CI
+# KOSLI_HOST_PRODUCTION is set in CI
+
+# - - - - - - - - - - - - - - - - - - -
+kosli_declare_pipeline()
+{
+  local -r hostname="${1}"
+
+  kosli pipeline declare \
+    --description "Ruby base image" \
+    --visibility public \
+    --template artifact,synk-scan \
+    --host "${hostname}"
+}
+
+# - - - - - - - - - - - - - - - - - - -
+kosli_report_artifact_creation()
+{
+  local -r hostname="${1}"
+
+  kosli pipeline artifact report creation \
+    "$(artifact_name)" \
+      --artifact-type docker \
+      --repo-root $(repo_root) \
+      --host "${hostname}"
+}
+
+# - - - - - - - - - - - - - - - - - - -
+kosli_report_synk_evidence()
+{
+  local -r hostname="${1}"
+
+  snyk container test "$(artifact_name)" \
+    --file="$(repo_root)/app/Dockerfile" \
+    --json-file-output=snyk.json \
+    --policy-path=.snyk
+
+  kosli pipeline artifact report evidence snyk \
+    "$(artifact_name)" \
+      --artifact-type docker \
+      --name snyk-scan \
+      --scan-results snyk.json \
+      --host "${hostname}"
+}
+
+# - - - - - - - - - - - - - - - - - - -
+kosli_assert_artifact()
+{
+  local -r hostname="${1}"
+
+  kosli assert artifact \
+    "$(artifact_name)" \
+      --artifact-type docker \
+      --host "${hostname}"
+}
+
+# - - - - - - - - - - - - - - - - - - -
+artifact_name()
+{
+  echo cyber-dojo/ruby-base
+}
+
+# - - - - - - - - - - - - - - - - - - -
+repo_root()
+{
+  # Functions in this file are called after sourcing this
+  # file so repo_root() cannot use the path of this script.
+  git rev-parse --show-toplevel
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - -
+on_ci()
+{
+  [ "${CI:-}" == true ]
+}
+
+# - - - - - - - - - - - - - - - - - - -
+on_ci_kosli_declare_pipeline()
+{
+  if on_ci ; then
+    kosli_declare_pipeline "${KOSLI_HOST_STAGING}"
+    kosli_declare_pipeline "${KOSLI_HOST_PRODUCTION}"
+  fi
+}
+
+# - - - - - - - - - - - - - - - - - - -
+on_ci_kosli_report_artifact()
+{
+  if on_ci ; then
+    kosli_report_artifact_creation "${KOSLI_HOST_STAGING}"
+    kosli_report_artifact_creation "${KOSLI_HOST_PRODUCTION}"
+  fi
+}
+
+# - - - - - - - - - - - - - - - - - - -
+on_ci_kosli_report_synk_evidence()
+{
+  if on_ci ; then
+    kosli_report_synk_evidence "${KOSLI_HOST_STAGING}"
+    kosli_report_snyk_evidence "${KOSLI_HOST_PRODUCTION}"
+  fi
+}
+
+# - - - - - - - - - - - - - - - - - - -
+on_ci_kosli_assert_artifact()
+{
+  if on_ci ; then
+    kosli_assert_artifact "${KOSLI_HOST_STAGING}"
+    kosli_assert_artifact "${KOSLI_HOST_PRODUCTION}"
+  fi
+}
+
+
+
